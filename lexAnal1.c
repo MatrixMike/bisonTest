0b3be8b8fd86d7268bf6bea761ef5233c759b783
Michael <matroxmike@icloud.com>
2023-02-12 19:12:23 +1100

    very first file

---
+++ lexAnal1.c
@@ -0,0 +1,27 @@
+/* Lexical analyzer returns a double floating point 
+   number on the stack and the token NUM, or the ASCII
+   character read if not a number.  Skips all blanks
+   and tabs, returns 0 for EOF. */
+
+#include <ctype.h>
+
+yylex ()
+{
+  int c;
+
+  /* skip white space  */
+  while ((c = getchar ()) == ' ' || c == '\t')  
+    ;
+  /* process numbers   */
+  if (c == '.' || isdigit (c))                
+    {
+      ungetc (c, stdin);
+      scanf ("%lf", &yylval);
+      return NUM;
+    }
+  /* return end-of-file  */
+  if (c == EOF)                            
+    return 0;
+  /* return single chars */
+  return c;                                
+}
